e12f5191494a55d543f08a65c60d75abb7559ce9
WilliamBerlin76 <william.berlin76@gmail.com>
2020-05-14 21:54:50 -0500

    added endpoint to update goal name

--- routes/goals-route.js
+++ routes/goals-route.js
@@ -13,7 +13,7 @@ router.post('/:cat_id/add', (req, res) => {
         })
         .catch(err => {
             res.status(500).json({error: 'The server failed to add the goal to the category.'})
-        })
+        });
 });
 
 // get goals by category id
@@ -25,6 +25,20 @@ router.get('/:cat_id', (req, res) => {
         })
         .catch(err => {
             res.status(500).json({error: 'The server failed to get the goals'})
+        });
+});
+
+// edit goal name referencing id in goal_user_cat table
+router.put('/:goal_id/update', (req, res) => {
+    const { name } = req.body;
+    const { goal_id } = req.params;
+
+    Goals.editCatGoal(goal_id, name)
+        .then(goal => {
+            res.status(200).json({message: "Goal name updated"})
+        })
+        .catch(err => {
+            res.status(500).json({error: "The server failed to update the goal name"})
         })
 })
 
