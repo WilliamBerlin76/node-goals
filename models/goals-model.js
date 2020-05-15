972d468c09dc205b6bd3eb374fc714d7ec8d01f5
WilliamBerlin76 <william.berlin76@gmail.com>
2020-05-14 19:39:49 -0500

    added helper function to get goals by category, with test

--- models/goals-model.js
+++ models/goals-model.js
@@ -1,7 +1,8 @@
 const db = require('../config/db-config')
 
 module.exports = {
-    addCatGoal
+    addCatGoal, 
+    getCatGoals
 }
 
 function addGoal(name){
@@ -9,6 +10,7 @@ function addGoal(name){
             .insert({name: name})
 }
 
+// adds a goal to the category
 async function addCatGoal(cat_id, goalName){
     const goal = await db('goals')
                         .where({name: goalName})
@@ -30,5 +32,26 @@ async function addCatGoal(cat_id, goalName){
                     user_cat_id: cat_id,
                     goal_id: newGoal.id
                 });
-    }
-}
+    };
+};
+
+async function getCatGoals(cat_id){
+    const category = await db('user_category as uc')
+                            .join('categories as c', 'uc.category_id', 'c.id')
+                            .select('uc.id as cat_id', 'c.name as category')
+                            .where({category_id: cat_id})
+                            .first()
+    if (category) {
+        const goals = await db('goal_user_cat as guc')
+                                .join('goals as g', 'g.id', 'guc.goal_id')
+                                .select(
+                                    'guc.id as id',
+                                    'g.name as name'
+                                )
+                                .where({user_cat_id: cat_id})
+        return {
+            ...category,
+            goals: goals
+        };
+    };
+};
--- tests/models/goals.test.js
+++ tests/models/goals.test.js
@@ -1,6 +1,6 @@
 const db = require('../../config/db-config.js');
 
-const { addCatGoal } = require('../../models/goals-model')
+const { addCatGoal, getCatGoals } = require('../../models/goals-model')
 
 describe("goals-models", () => {
     beforeEach(async () => {
@@ -34,4 +34,26 @@ describe("goals-models", () => {
             expect(catGoal[0].goal_id).toBe(1)
         });
     });
+
+    describe("getCatGoals", () => {
+        it('should retrieve the goals by cat_id', async() => {
+            await db('categories').insert({name: "test-cat2"});
+            await db('user_category').insert({
+                user_id: 1,
+                category_id: 2
+            });
+            await addCatGoal(1, "test-goal1");
+            await addCatGoal(1, "test-goal2");
+            await addCatGoal(1, "test-goal3");
+
+            await addCatGoal(2, "test-goal1.1");
+            await addCatGoal(2, "test-goal2.1");
+
+            const goals = await getCatGoals(1)
+            const goals2 = await getCatGoals(2)
+    
+            expect(goals.goals).toHaveLength(3)
+            expect(goals2.goals).toHaveLength(2)
+        })
+    })
 });
