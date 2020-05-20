d08d18a2a54922dc99e2d68d6bf75e134b303d72
WilliamBerlin76 <william.berlin76@gmail.com>
2020-05-19 20:31:42 -0500

    added helper function to get steps attached to a goal, with a test

--- models/steps-model.js
+++ models/steps-model.js
@@ -1,7 +1,8 @@
 const db = require('../config/db-config');
 
 module.exports = {
-    addStepToGoal
+    addStepToGoal,
+    getGoalSteps
 };
 
 function addStep(name){
@@ -32,5 +33,28 @@ async function addStepToGoal(goal_id, stepName, stepNum){
                     goal_user_cat_id: goal_id,
                     step_id: newStep.id
                 });
+    };
+};
+
+async function getGoalSteps(goal_id){
+    const goal = await db('goal_user_cat as guc')
+                        .join('goals as g', 'guc.goal_id', 'g.id')
+                        .select('guc.id as goal_id', 'g.name as goal')
+                        .where('guc.id', '=', goal_id)
+                        .first()
+    if (goal) {
+        const steps = await db('step_list as sl')
+                                .join('steps as s', 'sl.step_id', 's.id')
+                                .select(
+                                    'sl.id as id',
+                                    's.name as name',
+                                    'sl.step_num'
+                                )
+                                .orderBy('sl.step_num')
+                                .where({goal_user_cat_id: goal_id});
+        return {
+            ...goal,
+            steps: steps
+        };           
     };
 };
--- tests/models/steps.test.js
+++ tests/models/steps.test.js
@@ -1,6 +1,6 @@
 const db = require('../../config/db-config.js');
 
-const { addStepToGoal } = require('../../models/steps-model');
+const { addStepToGoal, getGoalSteps } = require('../../models/steps-model');
 
 describe("goals-models", () => {
     beforeEach(async () => {
@@ -9,6 +9,8 @@ describe("goals-models", () => {
         await db('goal_user_cat').truncate()
         await db('user_category').truncate();
         await db('users').truncate();
+        await db('step_list').truncate();
+        await db('steps').truncate();
         await db('users').insert({
             username: "thisisatest",
             email: "thisisatest@gmail.com",
@@ -28,7 +30,7 @@ describe("goals-models", () => {
 
     describe("addStepToGoal", () => {
         it('should add a step to the goal', async () => {
-            await addStepToGoal(1, 'test-step', 1)
+            await addStepToGoal(1, 'test-step', 1);
 
             const step = await db('steps');
             const goalStep = await db('step_list');
@@ -38,6 +40,24 @@ describe("goals-models", () => {
             expect(goalStep[0].goal_user_cat_id).toBe(1);
             expect(goalStep[0].step_id).toBe(1);
             expect(goalStep[0].step_num).toBe(1)
+        });
+    });
+
+    describe("getGoalSteps", () => {
+        it('should return the steps attached to a goal', async () => {
+            await addStepToGoal(1, 'test-step1', 1);
+            await addStepToGoal(1, 'test-step3', 3);
+            await addStepToGoal(1, 'test-step2', 2);
+
+            const stepList = await getGoalSteps(1);
+
+            expect(stepList.steps).toHaveLength(3);
+            // check if ordered by step_num
+            let stepNum = 1;
+            for(let i = 0; i < stepList.steps.length; i++){
+                expect(stepList.steps[i].step_num).toBe(stepNum);
+                stepNum++;
+            };
         });
     });
 });
